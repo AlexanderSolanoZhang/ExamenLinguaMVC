9f7eda0c4f386ca02dccd19b645cf9e0b0531131
AlexanderSolanoZhang <thexanderzhang@gmail.com>
2026-04-13 20:54:51 -0600

    R

--- Data/Migrations/20260414025442_InitialCreate.Designer.cs
+++ Data/Migrations/20260414025442_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExamenLinguaMVC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260414011703_InitialCreate0")]
-    partial class InitialCreate0
+    [Migration("20260414025442_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
